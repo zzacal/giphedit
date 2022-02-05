b1d4aa3534508c8e6d2a81849a7aa3f00e687468
zzacal <jizacal@gmail.com>
2022-02-04 22:40:46 -0800

    Game hub can handle multiple games

--- src/server/Controllers/PlayHub.cs
+++ src/server/Controllers/PlayHub.cs
@@ -1,3 +1,4 @@
+using Giphedit.Models;
 using Giphedit.Services;
 using Microsoft.AspNetCore.SignalR;
 
@@ -6,36 +7,38 @@ namespace Giphedit.Hubs;
 public class PlayHub : Hub
 {
   private readonly IGameService _games;
-  public PlayHub(ILogger<PlayHub> logger, IGameService gameService){
+  public PlayHub(ILogger<PlayHub> logger, IGameService gameService)
+  {
     _games = gameService;
   }
 
   public async Task Join(string gameId, string playerId)
   {
+    await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
     var result = await _games.JoinGame(gameId, playerId);
-    await Clients.All.SendAsync("ReceiveGame", result);
-  }
-  
-  public async Task SendMessage(string gameId, string playerId, string message)
-  {
-    await Clients.All.SendAsync("ReceiveMessage", gameId, playerId, message);
+    await SendGame(gameId, result);
   }
 
   public async Task Play(string gameId, string playerId, string cardId)
   {
     var result = await _games.Play(gameId, playerId, cardId);
-    await Clients.All.SendAsync("ReceiveGame", result);
+    await SendGame(gameId, result);
   }
 
   public async Task Judge(string gameId, string playerId, string cardId)
   {
     var result = await _games.Judge(gameId, playerId, cardId);
-    await Clients.All.SendAsync("ReceiveGame", result);
+    await SendGame(gameId, result);
   }
 
-  public async Task Start(string id, int turns, int handSize, string rating)
+  public async Task Start(string gameId, int turns, int handSize, string rating)
   {
-    var result = await _games.Start(id, turns, handSize, rating);
-    await Clients.All.SendAsync("ReceiveGame", result);
+    var result = await _games.Start(gameId, turns, handSize, rating);
+    await SendGame(gameId, result);
+  }
+
+  public async Task SendGame(string room, Game game)
+  {
+    await Clients.Group(room).SendAsync("ReceiveGame", game);
   }
 }

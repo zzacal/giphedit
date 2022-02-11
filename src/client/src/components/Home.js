92a074d3df2b581e53bb2982848cddc58f65a0b8
zzacal <jizacal@gmail.com>
2022-02-11 01:02:27 -0800

    Added loading states

--- src/client/src/components/Home.js
+++ src/client/src/components/Home.js
@@ -4,7 +4,6 @@ import services from "../services";
 import { Game } from "./game";
 import { JoinGame, Lobby, NewPlayer } from "./intro";
 
-
 const persist = services.getPersistence();
 const games = services.getGameService();
 const players = services.getPlayerService();
@@ -15,65 +14,86 @@ export const Home = () => {
   const [game, setGame] = useState(null);
   const [player, setPlayer] = useState(null);
   const [gameId, setGameId] = useState(null);
-    
+  const [isLoading, setIsLoading] = useState(null);
+
+  const wait = async (awaitable) => {
+    setIsLoading(true);
+    try{
+      await awaitable();
+    } finally {
+      setIsLoading(false);
+    }
+  };
   const onNewPlayer = async (name) => {
-    const result = await players.new(name);
-    persist.set("playerId", result.id);
-    setPlayer(result);
+    await wait(async () => {
+      const result = await players.new(name);
+      persist.set("playerId", result.id);
+      setPlayer(result);
+    });
   };
 
   const onNewGame = async () => {
-    const result = await games.get();
-    if(result) {
-      history.push(`/${result.name}`)
-      setGameId(result.id);
-    }
+    await wait(async () => {
+      const result = await games.get();
+      if (result) {
+        history.push(`/${result.name}`);
+        setGameId(result.id);
+      }
+    });
   };
 
   const onJoinGame = async (name) => {
-    if(name) {
-      const result = await games.find(name);
-      if(result) {
-        history.push(`/${result.name}`)
-        setGameId(result.id);
+    await wait(async () => {
+      if (name) {
+        const result = await games.find(name);
+        if (result) {
+          history.push(`/${result.name}`);
+          setGameId(result.id);
+        }
       }
-    }
+    });
   };
 
   const onStart = async (gameId, turns, hand, rating) => {
-    await hub.start(gameId, player.id, turns, hand, rating, setGame);
+    await wait(async () => {
+      await hub.start(gameId, player.id, turns, hand, rating, setGame);
+    });
   };
 
   const onPlay = async (cardId) => {
-    await hub.play(game.id, player.id, cardId, setGame);
+    await wait(async () => {
+      await wait(hub.play(game.id, player.id, cardId, setGame));
+    });
   };
 
   const onJudge = async (cardId) => {
-    await hub.judge(game.id, player.id, cardId, setGame);
+    await wait(async () => {
+      await hub.judge(game.id, player.id, cardId, setGame);
+    });
   };
-  
+
   const prstPlayerId = persist.get("playerId");
   if (!player && prstPlayerId) {
     players.get(prstPlayerId).then((result) => {
       result && setPlayer(result);
-    })
+    });
   }
 
   let params = useParams();
   const paramsGameName = params.gameName;
-  if(!gameId && paramsGameName) {
-    games.find(paramsGameName).then((result) => 
-      result && setGameId(result.id)
-    );
+  if (!gameId && paramsGameName) {
+    games.find(paramsGameName).then((result) => result && setGameId(result.id));
   }
 
   useEffect(() => {
-    if(!hub.isConnected && gameId && player?.id) {
+    if (!hub.isConnected && gameId && player?.id) {
       hub.join(gameId, player.id, setGame);
     }
-  }, [gameId, player?.id])
+  }, [gameId, player?.id]);
 
-  if (!player) {
+  if (isLoading) {
+    return <div class="text-center h2">☕️</div>;
+  } else if (!player) {
     return <NewPlayer onSubmit={onNewPlayer} />;
   } else if (!game) {
     return (
